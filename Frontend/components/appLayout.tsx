dff0f3da1e0c9242fe8fe20badc82838585f175f
KeyoungBae Kim <rudqo14@gmail.com>
2021-05-26 23:50:10 +0900

    Add: 네비게이션바 디자인 적용

--- Frontend/components/appLayout.tsx
+++ Frontend/components/appLayout.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import Link from "next/link";
 import "tailwindcss/tailwind.css";
 
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
-
 const AppLayout = ({ children }: AppLayout) => {
   return (
-    <nav>
-      <div className="flex space-x-2">
+    <nav className="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+      <div className="mb-2 sm:mb-0 inner">
         <Link href="/">
-          <a className="block px-4 py-2 rounded-md">Home</a>
+          <a className="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">
+            Home
+          </a>
         </Link>
+      </div>
+      <div className="sm:mb-0 self-center">
         <Link href="/profile">
-          <a className="block px-4 py-2 rounded-md">프로필</a>
+          <a className="">프로필</a>
         </Link>
         <Link href="/signup">
-          <a className="block px-4 py-2 rounded-md">회원가입</a>
+          <a className="">회원가입</a>
         </Link>
       </div>
       {children}
